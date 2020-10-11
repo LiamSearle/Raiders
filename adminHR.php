ae853aec018576b3fe5b1e8d49d4c8d6eb95fb21
ShadowScreamz <31037065+ShadowScreamz@users.noreply.github.com>
2020-10-11 14:35:57 +0200

    TSG

--- adminHR.php
+++ adminHR.php
@@ -8,34 +8,9 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
 </head>
-<?php
 
-// Check for the existence of longitude and latitude in our POST request
-if (isset($_POST['longitude']) && isset($_POST['latitude'])) 
-{
-	require_once('config.php');
-  // Cast variables to float 
-  $longitude = (float) $_POST['longitudes'];
-  $latitude = (float) $_POST['latitudes'];
-  // Set the timestamp from the current system time
-  $time = time();
-   $conn = mysqli_connect(SERVERNAME, USERNAME, PASSWORD, DATABASE)
-                or die("could not connect");
-  // Insert query into DB
-  $query = "INSERT INTO markers SET
-  'longitude' = {$longitude}, 
-  'latitude' = {$latitude},
-  'time' = {$time}";
-//execute the query 
-$result = mysqli_query($conn, $query)
-or die ("Error submitting location details");
-//let the user know the result of the action
-echo "<strong style= \"color:Red\">Location created successfully</strong>";
-//close the database
-mysqli_close($conn);
-header("Location:driver.php");
-?>
 <body>
+
 <a href="#" id = "get_location">Get Location</a>
 <div id = "map"> 
     <iframe id = "google_map" width = "425" height = "350" frameborder = "0" scrolling = "no" marginwidth = "0" 
@@ -48,23 +23,28 @@ header("Location:driver.php");
 //function to get dynamic user coordinates
 var location = function(position)
 {
-	var latitudes = position.coords.latitude,
+	$.post("adminHR.php",
+	{var latitudes = position.coords.latitude,
 		longitudes = position.coords.longitude,
 		coordinatess = latitudes + ',' + longitudes;
 		document.getElementById('google_map').setAttribute('src', 'https://maps.google.co.za/?q=' + coordinatess + '&z=60&output=embed');
-}
+},
 var errors = function(error)
 {
 	if(error.code === 1)
 	alert('location not found');
-}
+},
+var options = 
+{
+  enableHighAccuracy: true,
+  timeout: 5000,
+  maximumAge: 0;
+},
 document.getElementById('get_location').onclick = function()
 {
-	navigator.geolocation.getCurrentPosition(location, errors,
-	{enableHighAccuracy: true,
-	timeout: 15});
-	
+	navigator.geolocation.getCurrentPosition(location);
 	return false;
+});
 }
 </script>  
 </body>
