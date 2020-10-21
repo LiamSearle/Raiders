0629696d0d1ae93ff8320489d44800afa253ea61
ShadowScreamz <31037065+ShadowScreamz@users.noreply.github.com>
2020-10-21 14:13:52 +0200

    changes made to reporting

--- hrreports.php
+++ hrreports.php
@@ -23,6 +23,36 @@
      } 
 
 </script>
+ <!-- location button code -->
+<script>
+	function getGeoLocation()
+	{
+		if(navigator.geolocation)
+		{
+			navigator.geolocation.getCurrentPosition(onSuccess, onError);        
+		}
+	else {
+			console.log('Not Supported');
+		}
+	}
+	//fixed south african coordinates
+	function onSuccess(position)
+	{
+		const {latitude, longitude} = 
+		{
+			latitude: '34.6283864',
+			longitude: '27.2516951'
+		};
+		const url = `https://www.google.co.za/maps/?lat=${latitude}&amp;long=${longitude}`;
+		document.querySelector('a').setAttribute('href', url);
+		document.querySelector('div').style.display = 'block';
+	}
+ 
+	function onError(error)
+	{
+		console.log(error);
+	}
+	</script>
 </head>
 
 <body>
@@ -40,91 +70,35 @@
   </table> 
 </div> 
 
-<!-- creating the tab section  -->
-<div class="tab">
-  <button class="tablinks" onclick="openReport(event, 'Drivers')" id="defaultOpen">Drivers</button>
-  <button class="tablinks" onclick="openReport(event, 'Vehicles')">Vehicles</button>
-  <button class="tablinks" onclick="openReport(event, 'Location')">Location</button>
-</div>
 
 <!-- drivers tab -->
-<div id="Drivers" class="tabcontent">
+<div>
 <fieldset style="margin: auto; width: 100%;">
-
-<!-- navigation bar on drivers tab -->
-<div class="nav">
-    <table>
-                <tr>
-                 <td>
-                  
-                  <i class="fas fa-search"></i><input type="text" id="search" name="search" placeholder="Driver ID">
-                  <input type="submit" name="search" value="Search">
-                  
-                  </td>
-                </tr>
-    </table>
-</div>
-
-<!-- populating the drivers table with php -->
+<!-- populating the table with php -->
 <form action="hrreports.php" method="POST">
+
 <?php
 
 require_once("config.php");
   
 if(isset($_REQUEST['submit'])){
-  /* establish the connection to the database */
-  $conn = mysqli_connect(SERVERNAME, USERNAME, PASSWORD, DATABASE)
-      or die("there was an error connecting to the database.");
-
-  /* define the query */
-    $search=$_REQUEST['search'];
-    echo $search;
-  //want it to be ordered by client ID asc
-  $query = "SELECT * FROM driver WHERE driverID LIKE '%$search%'";
- 
-
-  /* get the results of the query and put them into a variable */
-  $result = mysqli_query($conn, $query)
-          or die("There was an error executing the query.");
-
-
-  while($row = mysqli_fetch_array($result)) {
-    $bookingID=$row['bookingID'];
-    $firstName=$row['firstName'];
-    $lastName=$row['lastName'];
-    $email=$row['emailAddress'];
-    $contactNumber=$row['contactNumber'];
-    $startDepot=$row['initialCollectionPoint'];
-    $endDepot=$row['finalCollectionPoint'];
-    $numberPassengers=$row['numberPassengers'];
-    $departureDate=$row['endDate'];
-  }
-
-  /* close the connection */
-  mysqli_close($conn);
-
-}
-//  /* import the config for the database */
-//  require_once("config.php");
-
-else{
  /* establish the connection to the database */
  $conn = mysqli_connect(SERVERNAME, USERNAME, PASSWORD, DATABASE)
      or die("there was an error connecting to the database.");
 
  /* define the query */
- $query = "SELECT driver.driverID, driver.firstName, driver.lastName, driver.contactNumber, driverlicence.licenceCode
+ $query = "SELECT driver.driverID, driver.firstName, driver.lastName, driver.contactNumber, 
+			driverlicence.licenceCode, vehicle.registrationNumber,vehicle.model,vehicle.numberOfSeats
   FROM driver
-  INNER JOIN driverlicence
-  ON driverlicence.driverID = driver.driverID
+  INNER JOIN driverlicence ON driverlicence.driverID = driver.driverID
+  INNER JOIN vehicle ON vehicle.licenceCode = licence.licenceCode
   ORDER BY driver.driverID ASC";
- 
 
  /* get the results of the query and put them into a variable */
  $result = mysqli_query($conn, $query)
          or die("There was an error executing the query.");
 
-  //creating drivers table
+  //creating table
   echo "<table style=\" border: 1px solid black; width: 100%;\">
   <tr>
   <th> DriverID </th>
@@ -132,6 +106,10 @@ else{
   <th> Last Name </th>
   <th> Contact Number </th>
   <th> Licence Code </th>
+  <th> Registration No </th>
+  <th> Model </th>
+  <th> Number of Seats </th>
+  <th>Location</th>
   </tr>";
   
    //displaying data
@@ -143,149 +121,25 @@ else{
      echo "<td>" . $row['lastName'] .  "</td>";
      echo "<td>" . $row['contactNumber'] . "</td>";
      echo "<td>" . $row['licenceCode'] ."</td>";
-     echo "</tr>";
-   }
-
-   echo "</table>";
-
-      //close the connection
-  mysqli_close($conn);
-}
-
-  ?>
-
-</fieldset>
-</div>    <!--end of drivers tab code -->
-
-
-<!-- depot tab -->
-<div id="Vehicles" class="tabcontent">
-<fieldset style="margin: auto; width: 100%;">
-<!-- navigation bar on drivers tab -->
-  <div class="nav">
-    <table>
-                <tr>
-                 <td><i class="fas fa-search"></i><input type="text" id="search" name="search" placeholder="Vehicle Registration No">
-                  <input type="submit" name="search" value="Search"></td>
-
-                </tr>
-    </table>
-</div>
-
-<!-- write code for vehicle table -->
-<form action="hrreports.php" method="POST">
-<?php
-
- /* import the config for the database */
- require_once("config.php");
-
- /* establish the connection to the database */
- $conn = mysqli_connect(SERVERNAME, USERNAME, PASSWORD, DATABASE)
-     or die("there was an error connecting to the database.");
-
- /* define the query */
- $query = "SELECT vehicle.registrationNumber,vehicle.model,vehicle.make,vehicle.numberOfSeats
-            FROM vehicle
-            ORDER BY vehicle.numberOfSeats ASC";
-
- /* get the results of the query and put them into a variable */
- $result = mysqli_query($conn, $query)
-         or die("There was an error executing the query.");
-
-
-
-  //creating vehicles table
-  echo "<table style=\" border: 1px solid black; width: 100%;\">
-  <tr>
-  <th> Registration No </th>
-  <th> Model </th>
-  <th> Make </th>
-  <th> Number of Seats </th>
-  </tr>";
-  
-   //displaying data
-   while($row=mysqli_fetch_array($result))
-   {
-     echo "<tr>";
-     echo "<td>" . $row['registrationNumber'] . "</td>";
+	 echo "<td>" . $row['registrationNumber'] . "</td>";
      echo "<td>" . $row['model'] . "</td>";
-     echo "<td>" . $row['make'] .  "</td>";
-     echo "<td>" . $row['numberOfSeats'] . "</td>"; 
-     echo "</tr>";
+     echo "<td>" . $row['numberOfSeats'] . "</td>";
+     echo  "<button onclick="getGeoLocation()">Find My Location</button>
+			<br><br>
+			<div style="display:none;">
+				<a href="#">Click Here></a>
+			</div>;
+	echo "</tr>";
    }
-
-   echo "</table>";
-
-      //close the connection
-  mysqli_close($conn);
-
-  ?>
-
-  <!-- write code for vehicle table -->
-<form action="hrreports.php" method="POST">
-<?php
-
- /* import the config for the database */
- require_once("config.php");
-
- /* establish the connection to the database */
- $conn = mysqli_connect(SERVERNAME, USERNAME, PASSWORD, DATABASE)
-     or die("there was an error connecting to the database.");
-
- /* define the query */
- $query = "SELECT daytripdepot.startDepotID,daytripdepot.endDepotID
-            FROM daytripdepot";
-
- /* get the results of the query and put them into a variable */
- $result = mysqli_query($conn, $query)
-         or die("There was an error executing the query.");
-
-
-  //creating location table
-  echo "<table style=\" border: 1px solid black; width: 100%;\">
-  <tr>
-  <th> Initial Destination </th>
-  <th> Final Destination </th>
-  </tr>";
-  
-   //displaying data
-   while($row=mysqli_fetch_array($result))
-   {
-     echo "<tr>";
-     echo "<td>" . $row['startDepotID'] . "</td>";
-     echo "<td>" . $row['endDepotID'] . "</td>";
-     echo "</tr>";
-   }
-   echo "</table>";
-
-      //close the connection
-  mysqli_close($conn);
+	echo "</table>";
+	//close the connection
+	mysqli_close($conn);
+}
 
   ?>
 
 </fieldset>
-</div> <!-- end of vehicle tab code -->
-
-<!-- javascript code to control the tabs  -->
-<script>
-function openReport(evt, report) {
-  var i, tabcontent, tablinks;
-  tabcontent = document.getElementsByClassName("tabcontent");
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
-  }
-  tablinks = document.getElementsByClassName("tablinks");
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "");
-  }
-  document.getElementById(report).style.display = "block";
-  evt.currentTarget.className += " active";
-}
-// Get the element with id="defaultOpen" and click on it
-document.getElementById("defaultOpen").click();
-</script>
-
-
+</div>    
 
  <!-- general footer code  -->
  <div class="footer"> 
